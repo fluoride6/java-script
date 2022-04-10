23f86c8c9afef6233886c712ff786cf08f65511b
Anna N <pirogcherniki@gmail.com>
2022-04-10 22:14:18 +0300

    Added files"

---
+++ basics/functions.js
@@ -0,0 +1,23 @@
+function getMax(arr) {
+    let max = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > max) max = arr[i];
+    }
+    return max;
+}
+
+function getMin(arr) {
+    let min = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < min) min = arr[i];
+    }
+    return min;
+}
+
+function getSum(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
